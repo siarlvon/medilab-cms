8863296db4e60804bea0a0ae916efb662da79fc1
siarlvon <111246007+siarlvon@users.noreply.github.com>
2023-03-08 12:25:22 -0500

    Fix structure

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
-import '@/styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@/styles/globals.css';
+import '@/styles/hero.css';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps}) {
+  return <Component {...pageProps} />;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,18 @@
-import { Inter } from 'next/font/google';
+import {Inter} from 'next/font/google';
 import styles from '@/styles/Home.module.css';
-import { gql, GraphQLClient } from 'graphql-request';
+import {gql, GraphQLClient} from 'graphql-request';
 import WelcomeSection from '@/components/WelcomeSection';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({subsets: ['latin']});
 
-export default function Home({ welcome }) {
+export default function Home({welcome}) {
   console.log(welcome);
   return (
     <>
       <div>
-        {welcome.welcomeDetail.map(section => <WelcomeSection details={section} key={section.id} />)}
+        {welcome.welcomeDetail.map((section) => (
+          <WelcomeSection details={section} key={section.id} />
+        ))}
       </div>
     </>
   );
@@ -23,7 +25,7 @@ const query = gql`
       name
       slug
       welcomeDetail {
-        ...on PageHeaderRecord {
+        ... on PageHeaderRecord {
           __typename
           title
           buttonText
@@ -33,20 +35,20 @@ const query = gql`
       }
     }
   }
-`
+`;
 
 export async function getStaticProps() {
-  const endpoint = "https://graphql.datocms.com/";
+  const endpoint = 'https://graphql.datocms.com/';
   const client = new GraphQLClient(endpoint, {
     headers: {
-      "content-type": "application/json",
-      authorization: "Bearer " + process.env.DATOCMS_API_KEY,
-    }
+      'content-type': 'application/json',
+      authorization: 'Bearer ' + process.env.DATOCMS_API_KEY,
+    },
   });
 
   const welcome = await client.request(query);
   console.log(welcome);
   return {
-    props: welcome
-  }
+    props: welcome,
+  };
 }
